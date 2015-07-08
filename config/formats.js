430e8270df946e40404cf09924095f7807173498
miahjennatills <alexdipper@hotmail.co.uk>
2015-07-08 19:04:38 +0100

    added mod for NFE

--- config/formats.js
+++ config/formats.js
@@ -4380,10 +4380,10 @@ exports.Formats = [
 			if (team.length < 4) return ['You must bring at least three Pokémon.'];
 		},
 		onBegin: function () {
-			this.debug('cutting down to 3');
-			this.p1.pokemon = this.p1.pokemon.slice(0, 3);
+			this.debug('cutting down to 4');
+			this.p1.pokemon = this.p1.pokemon.slice(0, 4);
 			this.p1.pokemonLeft = this.p1.pokemon.length;
-			this.p2.pokemon = this.p2.pokemon.slice(0, 3);
+			this.p2.pokemon = this.p2.pokemon.slice(0, 4);
 			this.p2.pokemonLeft = this.p2.pokemon.length;
 			}
 		},
@@ -4397,20 +4397,21 @@ exports.Formats = [
 		banlist: ['Tornadus + Defiant', 'Thundurus + Defiant', 'Landorus + Sheer Force'],
 		requirePentagon: true,
 		validateTeam: function (team, format) {
-			if (team.length < 3) return ['You must bring at least three Pokémon.'];
+			if (team.length < 4) return ['You must bring at least three Pokémon.'];
 		},
 		onBegin: function () {
 			this.debug('cutting down to 4');
-			this.p1.pokemon = this.p1.pokemon.slice(0, 3);
+			this.p1.pokemon = this.p1.pokemon.slice(0, 4);
 			this.p1.pokemonLeft = this.p1.pokemon.length;
-			this.p2.pokemon = this.p2.pokemon.slice(0, 3);
+			this.p2.pokemon = this.p2.pokemon.slice(0, 4);
 			this.p2.pokemonLeft = this.p2.pokemon.length;
 			}
 		},
 {
 		name: "NFE",
 		section: "ORAS Singles",
-
+		
+		mod: 'NFE'
 		ruleset: ['Pokemon', 'Species Clause', 'Moody Clause', 'Baton Pass Clause', 'Evasion Moves Clause', 'OHKO Clause',
 			'Swagger Clause', 'Endless Battle Clause', 'Team Preview', 'HP Percentage Mod', 'Sleep Clause Mod', 'Cancel Mod'
 		],
